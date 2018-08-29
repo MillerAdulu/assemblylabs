b2fe1d7c0359c5f56c9ea29a280c7b14464714e1
Miller Adulu <milleradulu@gmail.com>
2018-08-29 11:58:01 +0300

    Used constants for system calls

--- 0 Hello/helloworld.asm
+++ 0 Hello/helloworld.asm
@@ -1,5 +1,7 @@
 section .data
 
+SYS_EXIT EQU 1
+SYS_WRITE EQU 4
 msg db 'Hello world', 0xa ; String to be edited
 msgMine db 'This is my first assembly program', 0xa
 len equ $ - msg ; Define the length of the string
@@ -12,13 +14,13 @@ section .text
 
     mov edx, len ; Move message length to register edx
     mov ecx, msg ; Move message to register ecx
-    mov ebx, 1   ; File descriptor(stdout)
-    mov eax, 4   ; System call (system write)
+    mov ebx, SYS_EXIT  ; File descriptor(stdout)
+    mov eax, SYS_WRITE   ; System call (system write)
     int 0x80     ; Kernel call
 
     mov edx, lenMine
     mov ecx, msgMine
-    mov eax, 4
+    mov eax, SYS_WRITE
 
-    mov eax, 1   ; System call (exit)
+    mov eax, SYS_EXIT   ; System call (exit)
     int 80h      ; Kernel call
