ce57a5cc73902ed84d52e46105d498b2047f70af
Miller Adulu <milleradulu@gmail.com>
2018-09-05 11:13:04 +0300

    Basic user input

---
+++ 2 User Input/userinput.asm
@@ -0,0 +1,36 @@
+section .data
+
+  SYS_EXIT EQU 1
+  SYS_READ EQU 3
+  SYS_WRITE EQU 4
+
+  PROMPT DD "Enter your name: "
+
+section .bss
+
+  myName RESW 10
+
+section .text
+
+  global _start
+  _start:
+
+  mov eax, SYS_WRITE
+  mov ebx, SYS_EXIT
+  mov ecx, PROMPT
+  mov edx, 19
+  int 0x80
+
+  mov eax, SYS_READ
+  mov ebx, SYS_EXIT
+  mov ecx, myName
+  mov edx, 23
+  int 0x80
+
+  mov eax, SYS_WRITE
+  mov ebx, SYS_EXIT
+  mov ecx, myName
+  int 0x80
+
+  mov eax, SYS_EXIT
+  int 0x80
